f2ed1b6e1998eef4485a620eae30cd8d9996ccb5
Waine Junior <waine2000@gmail.com>
2019-04-22 14:35:23 -0300

    Updated CRUD files

--- 05 - Cadastro - CRUD/crud_commands.py
+++ 05 - Cadastro - CRUD/crud_commands.py
@@ -1,17 +1,11 @@
 '''
 COMMANDS = {
-	OPERATION_1: {
+	COMMAND_NAME: {
 		'cmd': command, 
 		'fmt': command format,
+		're_inp': ReGex arguments format,
 		'decr':command description
-	},
-	OPERATION_2: {
-		'cmd': command, 
-		'fmt': command format,
-		're_inp': ReGex input format,
-		'decr': command description			
-	},
-	...
+	}, ...
 }
 '''
 
@@ -30,7 +24,7 @@ CMDS = {
 	},
 	'UPDATE':{
 		'cmd':'-u',
-		'fmt':'[LOGIN || NAME] ; NEW_NAME ; NEW_HOMETOWN',
+		'fmt':'LOGIN || NAME ; NEW_NAME ; NEW_HOMETOWN',
 		're_inp':[['-u'],['[0-9]+','.+'],[';'],['.+'], [';'], ['.+']],
 		'dcr':'update person on database'
 	},
@@ -42,7 +36,7 @@ CMDS = {
 	},
 	'ADD_FRIEND':{
 		'cmd':'-a',
-		'fmt':'[LOGIN_1 || NAME_1] ; [LOGIN_2 || NAME_2]',
+		'fmt':'LOGIN_1 || NAME_1 ; LOGIN_2 || NAME_2',
 		're_inp':[['-a'],['[0-9]+','.+'],[';'],['[0-9]+','.+']],
 		'dcr':'Adds friend relationship'		
 	},
@@ -59,3 +53,87 @@ CMDS = {
 		'dcr':'Prints commands, its format and description'
 	}
 }
+
+
+def HELP(args, db):
+	'''
+		Prints all commands format and description
+		args[0]: '-h'
+		db: database
+	'''	
+	sorted_cmds = []	
+	for i in CMDS:
+		sorted_cmds.append(i)
+	sorted_cmds.sort()
+	for i in sorted_cmds: # prints commands in alphabetic order
+		print(i +": " + CMDS[i]['cmd'] + " " + CMDS[i]['fmt'])
+		print(CMDS[i]['dcr'] + "\n")
+
+
+def QUIT(args, db):
+	'''
+		Does things that has to do on quiting
+		args[0]: '-q'
+		db: database
+	'''
+	print("Quitting...")	
+	a = 0
+
+
+def ADD_FRIEND(args, db):
+	'''
+		Add friend relationship to database if possible
+		args[0]: '-a'
+		args[1]: NAME OR LOGIN of person 1
+		args[2]: NAME OR LOGIN of person 2
+		db: database
+	'''
+	print("Adding friends relationship...")
+	a = 0
+
+
+def LIST(args, db):
+	'''
+		Prints list of persons on database
+		args[0]: '-l'
+		db: database
+	'''
+	print("Listing persons...")
+	a = 0
+
+
+def UPDATE(args, db):
+	'''
+		Update person on database
+		args[0]: '-u'
+		args[1]: NAME or LOGIN of person to update
+		args[2]: new person's name
+		args[3]: new person's hometown
+		db: database
+	'''
+	print("Updatin person...")
+	a = 0
+
+
+def DELETE(args, db):
+	'''
+		Delete person of database
+		args[0]: '-d'
+		args[1]: NAME or LOGIN of person to delete
+		db: database
+	'''
+	print("Deleting person...")
+	a = 0
+
+
+def INSERT(args, db):
+	'''
+		Insert person on database
+		args[0]: '-i'
+		args[1]: person's name
+		args[2]: person's hometown
+		db: database
+	'''
+	print("Inserting person...")
+	a = 0
+
--- 05 - Cadastro - CRUD/main.py
+++ 05 - Cadastro - CRUD/main.py
@@ -1,6 +1,26 @@
 from crud_commands import *
+import re
 
-def parse_input(inp):	
+def check_regex(parse, regex):
+	'''
+		Check if parsed input is valid, given ReGex format
+		regex(list(list())): list of list of valids formats
+		parse(list()): parsed input
+	'''
+	if(len(regex) == len(parse)):
+		for i in range(0, len(regex)):
+			find = False
+			for fmt in regex[i]:
+				x = re.search(fmt, parse[i])
+				if x != None:
+					find = True
+			if find == False:
+				return False
+		return True
+	else:
+		return False
+
+def parse_input(inp):
 	inp = inp.split(" ", 1)
 	if(inp[0] == inp[-1]): # if the input has no arguments
 		return [inp[0]]
@@ -10,45 +30,81 @@ def parse_input(inp):
 	for arg in args:
 		p_inp.append(arg)
 		p_inp.append(';') # insert ';' between arguments
-	p_inp.pop()
+	p_inp.pop() # remove last ';'
 	return (p_inp)
 
 def check_format(inp):
 	try:
-		if(inp[0] in CMDS[:]['cmd']):
-			if(inp[0] == CMDS['HELP']):
-				 
+		if inp[0] == CMDS['ADD_FRIEND']['cmd']:
+			if(check_regex(inp, CMDS['ADD_FRIEND']['re_inp'])):
+				return 1
+			else:
+				return -1
+		elif inp[0] == CMDS['DELETE']['cmd']:
+			if(check_regex(inp, CMDS['DELETE']['re_inp'])):
+				return 1
+			else:
+				return -1
+
+		elif inp[0] == CMDS['HELP']['cmd']:
+			if(check_regex(inp, CMDS['HELP']['re_inp'])):
+				return 1
+			else:
+				return -1
+
+		elif inp[0] == CMDS['INSERT']['cmd']:
+			if(check_regex(inp, CMDS['INSERT']['re_inp'])):
+				return 1
+			else:
+				return -1
+
+		elif inp[0] == CMDS['LIST']['cmd']:
+			if(check_regex(inp, CMDS['LIST']['re_inp'])):
+				return 1
+			else:
+				return -1
+
+		elif inp[0] == CMDS['QUIT']['cmd']:
+			if(check_regex(inp, CMDS['QUIT']['re_inp'])):
+				return 1
+			else:
+				return -1
+
+		elif inp[0] == CMDS['UPDATE']['cmd']:	 
+			if(check_regex(inp, CMDS['UPDATE']['re_inp'])):
+				return 1
+			else:
+				return -1
+
 		else:
-			print('Invalid input format'):
+			print('Invalid input format')
 			return -1
 
 	except Exception as e:
-		print('Invalid input format')
+		print('Excepction')
 		return -1
 
 if __name__ == '__main__':
 	inp = ''
 	while(1): # quits when inp = CMDS['QUIT']['cmd']
-		inp = raw_input():
-
+		inp = input()
+		db = 0
 		# Parse input string
 		inp = parse_input(inp)
-
 		# Check if list of strings is valid
-		if(check_formats(inp) == 1):
+		if(check_format(inp) == 1):
 			if(inp[0] == CMDS['INSERT']['cmd']):
-			
+				INSERT(inp, db)
 			elif(inp[0] == CMDS['DELETE']['cmd']):
-
+				DELETE(inp, db)
 			elif(inp[0] == CMDS['UPDATE']['cmd']):
-
+				UPDATE(inp, db)
 			elif(inp[0] == CMDS['LIST']['cmd']):
-
+				LIST(inp, db)
 			elif(inp[0] == CMDS['ADD_FRIEND']['cmd']):
-
+				ADD_FRIEND(inp, db)
 			elif(inp[0] == CMDS['QUIT']['cmd']):
-
+				QUIT(inp, db)
+				break
 			elif(inp[0] == CMDS['HELP']['cmd']):
-
-
-		
+				HELP(inp, db)
