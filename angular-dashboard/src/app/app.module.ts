1607334130d57b3828cf759ed61a369fb76c590e
Ian <ianqueros.1996@alunos.utfpr.edu.br>
2019-06-11 11:41:16 -0300

    Adicionando projeto angular

---
+++ angular-dashboard/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout'
+
+import { CustomMaterialModule } from './custom-material/custom-material.module';
+import { InitialPageComponent } from './initial-page/initial-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TriviaComponent } from './trivia/trivia.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InitialPageComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'trivia',
+    component: TriviaComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InitialPageComponent,
+    DashboardComponent,
+    TriviaComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    CustomMaterialModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
