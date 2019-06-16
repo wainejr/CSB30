16f2248127252a89ab25e72b3cf405e3498e53af
NEOGIGIO <gigio.forastieri@gmail.com>
2019-06-15 23:54:57 -0300

    new routes

--- server/sandbox/server/app.py
+++ server/sandbox/server/app.py
@@ -1,16 +1,25 @@
+# source bin/activate
+
 from flask import Flask
 from flask import jsonify
 from supergenre_handler import *
 		
 app = Flask(__name__)
 
-@app.route('/all_genres')
+@app.route('/chart/all_genres')
 def index():
-    return jsonify({"genres": get_all_genres()})
+    return jsonify(get_all_genres())
 
-@app.route('/all_genres')
-def index():
-    return jsonify({"genres": get_all_genres()})
+@app.route('/chart/count_supergenres')
+def count_supergenres():
+    labels = []
+    data = []
+
+    for lista in get_count_bands_in_supergenres():
+        labels.append(lista[0])
+        data.append(lista[1])
+
+    return jsonify({"labels": labels, "data": data})
 
 if __name__ == '__main__':
     app.run(debug=True)
